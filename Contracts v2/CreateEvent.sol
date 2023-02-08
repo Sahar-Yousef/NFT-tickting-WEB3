515dfc19f250b27fff96cbbcffc804f4fb95a86c
Mashael <108432621+Mashael-M@users.noreply.github.com>
2023-02-08 11:23:34 +0300

    Update and rename Lab9.cpp to CreateEvent.sol

---
+++ Contracts v2/CreateEvent.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+import "./MyToken.sol";
+
+
+
+contract CreateEvent {
+    address public Organizer;
+    MyToken _MyToken;//spling
+    // Contract_B public contractB_ref;
+
+    constructor() {
+        Organizer = msg.sender;
+    }
+
+   struct Event {
+    uint id;
+    address owner;
+    string name;
+    uint date; 
+    uint ticketPrice;
+    uint ticketCount; 
+    uint ticketRemaining;
+    uint Max;
+  }
+  mapping(uint => Event) public events;
+  mapping(address => mapping(uint => uint)) public tickets;
+  uint public eventId ;
+                            
+  event EventLog ( address Organizer, uint id);
+
+  // add max here 
+                            
+  function createEvent( string calldata eventName, uint eventDate, uint price, uint eventCapacity, uint Max) external  {
+    require(eventCapacity > 0, 'Minimum capacity (ticket count) should be 1.'); // using interface
+    require( msg.sender == Organizer ); // to check organizer address
+    events[eventId] = Event(eventId, msg.sender, eventName, eventDate, price, eventCapacity, eventCapacity, Max);
+    emit EventLog ( msg.sender, eventId);
+    eventId++;
+    // obj 
+    
+          
+  }
+
+                            
+  function buyTicket(uint id, address _to, uint NumOfTicket) eventExists(id)  payable external {
+    require(msg.value >= (events[id].ticketPrice * NumOfTicket), 'Not enough ETH is sent to pay the tickets.');
+    require(NumOfTicket <= 2, 'This is Maximum limit'); // for this address check
+    require(tickets[msg.sender][id] <= NumOfTicket  , 'Number of ticket is max');//
+    require(events[id].ticketRemaining >= NumOfTicket, 'Not enough tickets.');
+
+    events[id].ticketRemaining -= NumOfTicket;
+    tickets[msg.sender][id] += NumOfTicket; //++
+   
+    // Creat it as NFT Tick  
+    // OpenZipplen 
+    _MyToken.safeMint(_to, NumOfTicket);
+  }
+
+
+  function getAllEvents() external view returns (Event[] memory){
+    Event[] memory eventArray = new Event[](eventId);
+    for (uint i = 0; i < eventId; i++) {
+        eventArray[i] = events[i];
+    }
+    return eventArray;
+  }
+
+  modifier eventExists(uint id) {
+    require(events[id].date != 0, 'Event does not exist.');
+    _;
+  }
+
+
+  //address
+
+   // Users Struct
+    struct UserStruct {
+        uint index;
+    }
+  
+    // Mapping users to addresss
+    mapping(address => UserStruct) private userStructs;
+    address[] private userIndex;
+
+    // New user event
+    event LogNewUser (address indexed userAddress, uint index);
+
+}
