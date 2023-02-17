0cb38f2ce141dbeb81c9e8644c7010e1b6953264
Mashael <108432621+Mashael-M@users.noreply.github.com>
2023-02-17 23:56:09 +0300

    Update CreateEvent.sol

--- Contract V3/CreateEvent.sol
+++ Contract V3/CreateEvent.sol
@@ -9,38 +9,38 @@ contract CreateEvent {
     
     MyToken _MyToken = new MyToken();
     
-    // Contract_B public contractB_ref;
+    
 
     constructor() {
         Organizer = msg.sender;
-        // MyToken = 
+         
     }
 
    struct Event {
     uint id;
     address owner;
-    string name; // byte
-    uint date; // timestap 
+    string name; 
+    uint date; 
+    uint time;
     uint ticketPrice;
     uint ticketCount;
     uint ticketRemaining;
-    uint Max;
+    string EventDescription;
   }
   mapping(uint => Event) public events;
   mapping(address => mapping(uint => uint)) public tickets;
   uint public eventId ;
-                         //= 100    
+                            
   event EventLog ( address Organizer, uint id);
 
-  // add max here 
-                            //memory
-  function createEvent( string calldata eventName, uint eventDate, uint price, uint eventCapacity, uint Max) external  {
+  
+  function createEvent( string calldata eventName, uint eventDate, uint eventTime, uint price, uint eventCapacity, string calldata EventDescription) external  {
     require(eventCapacity > 0, 'Minimum capacity (ticket count) should be 1.'); // using interface
     require( msg.sender == Organizer ); // to check organizer address
-    events[eventId] = Event(eventId, msg.sender, eventName, eventDate, price, eventCapacity, eventCapacity, Max);
+    events[eventId] = Event(eventId, msg.sender, eventName, eventDate, eventTime, price, eventCapacity, eventCapacity, EventDescription);
     emit EventLog ( msg.sender, eventId);
     eventId++;
-    // obj 
+     
     
           
   }
@@ -49,7 +49,7 @@ contract CreateEvent {
   function buyTicket(uint id, address _to, uint256 NumOfTicket) eventExists(id)  payable external {
     require(msg.value >= (events[id].ticketPrice * NumOfTicket), 'Not enough ETH is sent to pay the tickets.');
     
-    require(tickets[msg.sender][id] <= 2  , 'You have reached the Maximum amount of tickets');//
+    require(tickets[msg.sender][id] <= 3  , 'You have reached the Maximum amount of tickets');//
     require(NumOfTicket <= 2, 'This is Maximum limit'); // for this address check
     require(events[id].ticketRemaining >= NumOfTicket, 'Sorry the tickets are sold out!');
 
@@ -57,11 +57,8 @@ contract CreateEvent {
     tickets[msg.sender][id] += NumOfTicket; //++
    
     _MyToken.safeMint(_to, NumOfTicket);
-    // Creat it as NFT Tick  
-    // OpenZipplen 
-    //_MyToken.safeMint(_to, NumOfTicket);
+   
   }
-
 
   function getAllEvents() external view returns (Event[] memory){
     Event[] memory eventArray = new Event[](eventId);
@@ -72,7 +69,7 @@ contract CreateEvent {
   }
 
   modifier eventExists(uint id) {
-    require(events[id].date != 0, 'Event does not exist.');
+    require(events[id].date != 0 , 'Event does not exist.');
     _;
   }
 
